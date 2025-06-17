eab58e5f7821c7f70bca16ffd0ee29bbe6eaaf62
Lirk133 <lirk133@gmail.com>
2025-06-17 21:09:06 +0300

    configure picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ application { mainClass.set("hexlet.code.App") }
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.7")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,28 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+//import picocli.CommandLine.Option;
+//import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", version = "gendiff pre-alfa 0.0.0.1", mixinStandardHelpOptions = true,
+        description = "Сравнивает два конфигурационных файла и показывает разницу.")
+public class App implements Runnable {
+
+//    @Option(names = { "-s", "--font-size" }, description = "Font size")
+//    int fontSize = 19;
+
+//    @Parameters(paramLabel = "<word>", defaultValue = "Hello, picocli",
+//            description = "Words to be translated into ASCII art.")
+//    private String[] words = { "Hello,", "picocli" };
+
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
