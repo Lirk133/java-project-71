988dff7d728bd34473c17182e92cb6d6fe879331
Lirk133 <lirk133@gmail.com>
2025-07-05 19:12:35 +0300

    edit Low code

--- app/src/test/java/hexlet/code/TestDiffer.java
+++ app/src/test/java/hexlet/code/TestDiffer.java
@@ -2,54 +2,58 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDiffer {
 
     @Test
-    public void testNormal1() throws IOException {
+    public void testNormal1() {
         String file1 = "../app/src/test/resources/file1.json";
         String file2 = "../app/src/test/resources/file2.json";
-        String result = "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n";
-        assertEquals(Differ.generate(file1, file2), result);
+        String result = """
+                  - follow: false
+                    host: hexlet.io
+                  - proxy: 123.234.53.22
+                  - timeout: 50
+                  + timeout: 20
+                  + verbose: true
+                """;
+        assertEquals(result, Differ.generate(file1, file2));
     }
 
     @Test
-    public void testNormal2() throws IOException {
+    public void testNormal2() {
         String file1 = "../app/src/test/resources/file2.json";
         String file2 = "../app/src/test/resources/file1.json";
-        String result = "  + follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  + proxy: 123.234.53.22\n"
-                + "  - timeout: 20\n"
-                + "  + timeout: 50\n"
-                + "  - verbose: true\n";
-        assertEquals(Differ.generate(file1, file2), result);
+        String result = """
+                  + follow: false
+                    host: hexlet.io
+                  + proxy: 123.234.53.22
+                  - timeout: 20
+                  + timeout: 50
+                  - verbose: true
+                """;
+        assertEquals(result, Differ.generate(file1, file2));
     }
 
     @Test
-    public void testEqual() throws IOException {
+    public void testEqual() {
         String file1 = "../app/src/test/resources/file1.json";
         String file2 = "../app/src/test/resources/file1.json";
-        String result = "    follow: false\n"
-                + "    host: hexlet.io\n"
-                + "    proxy: 123.234.53.22\n"
-                + "    timeout: 50\n";
-        assertEquals(Differ.generate(file1, file2), result);
+        String result = """
+                    follow: false
+                    host: hexlet.io
+                    proxy: 123.234.53.22
+                    timeout: 50
+                """;
+        assertEquals(result, Differ.generate(file1, file2));
     }
 
     @Test
-    public void testEmpty() throws IOException {
+    public void testEmpty() {
         String file1 = "";
         String file2 = "";
         String result = "";
-        assertEquals(Differ.generate(file1, file2), result);
+        assertEquals(result, Differ.generate(file1, file2));
     }
 }
