dfd5d5a965539f804b1e6fcbcbf4751063516583
Lirk133 <lirk133@gmail.com>
2025-07-05 19:27:49 +0300

    edit

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -12,7 +12,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Differ {
+class Differ {
 
     public static String generate(String filePath1, String filePath2) {
 
--- app/src/test/java/hexlet/code/TestDiffer.java
+++ app/src/test/java/hexlet/code/TestDiffer.java
@@ -10,7 +10,7 @@ public class TestDiffer {
     public void testNormal1() {
         String file1 = "../app/src/test/resources/file1.json";
         String file2 = "../app/src/test/resources/file2.json";
-        String result = """
+        String result1 = """
                   - follow: false
                     host: hexlet.io
                   - proxy: 123.234.53.22
@@ -18,14 +18,7 @@ public class TestDiffer {
                   + timeout: 20
                   + verbose: true
                 """;
-        assertEquals(result, Differ.generate(file1, file2));
-    }
-
-    @Test
-    public void testNormal2() {
-        String file1 = "../app/src/test/resources/file2.json";
-        String file2 = "../app/src/test/resources/file1.json";
-        String result = """
+        String result2 = """
                   + follow: false
                     host: hexlet.io
                   + proxy: 123.234.53.22
@@ -33,20 +26,15 @@ public class TestDiffer {
                   + timeout: 50
                   - verbose: true
                 """;
-        assertEquals(result, Differ.generate(file1, file2));
-    }
-
-    @Test
-    public void testEqual() {
-        String file1 = "../app/src/test/resources/file1.json";
-        String file2 = "../app/src/test/resources/file1.json";
-        String result = """
+        String result3 = """
                     follow: false
                     host: hexlet.io
                     proxy: 123.234.53.22
                     timeout: 50
                 """;
-        assertEquals(result, Differ.generate(file1, file2));
+        assertEquals(result1, Differ.generate(file1, file2));
+        assertEquals(result2, Differ.generate(file2, file1));
+        assertEquals(result3, Differ.generate(file1, file1));
     }
 
     @Test
